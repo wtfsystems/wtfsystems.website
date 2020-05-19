bb59e3a7391542bb70e1cc02c51f5bc7a636b548
wtfsystems <mevans+git@wtfsystems.net>
2020-05-18 20:39:59 -0400

    sync

---
+++ copy_docs.bat
@@ -0,0 +1,6 @@
+@echo off
+SETLOCAL
+
+robocopy "C:\Users\Matthew\Documents\Projects\wtengine\documentation\html" "C:\Users\Matthew\Documents\Projects\wtfsystems.github.io\docs\wtengine" /E /PURGE
+
+ENDLOCAL
