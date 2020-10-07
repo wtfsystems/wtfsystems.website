135ddafcf07fead0c81cf5b69fb6c2b157fb35a9
wtfsystems <mevans+git@wtfsystems.net>
2020-10-07 13:58:25 -0400

    sync

--- prime_wheel.js
+++ prime_wheel.js
@@ -128,18 +128,6 @@ function toggle_background() {
 }
 
 /*
- * Check to see if the canvas exists
- */
-function canvas_exists() {
-    try {
-        var pixel = ctx.getImageData(0, 0, 1, 1);
-        return false;
-    } catch(err) {
-        return (err.code === 18);
-    }
-}
-
-/*
  * CSS:
  * html {
  *     height: 100%;
