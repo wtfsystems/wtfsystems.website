f2c865c699f9bd06a82c3b494551f3c1f5bed9a5
wtfsystems <mevans+git@wtfsystems.net>
2020-10-10 17:59:58 -0400

    sync

--- docs/http_auth_framework/search/pages_1.js
+++ docs/http_auth_framework/search/pages_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['python_2fflask_20web_20app_20framework_41',['Python/Flask Web App Framework',['../index.html',1,'']]]
+  ['python_2fflask_20web_20authentication_20framework_41',['Python/Flask Web Authentication Framework',['../index.html',1,'']]]
 ];
