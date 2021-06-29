f3bae11a7275ce5fb9913377aa7f8196257ef467
Matthew Evans <mevans+git@wtfsystems.net>
2021-06-28 23:47:30 -0400

    issue with primewheel, will fix later

--- src/main.js
+++ src/main.js
@@ -24,18 +24,18 @@ Prism.manual = true  //  Disable PrismJS's auto highlighting.
 import cmdProcessor from './modules/TermProcessor.mjs'
 import termRenderer from './modules/TermRenderer.mjs'
 import { PostRenderer } from './modules/PostRenderer.mjs'
-import { PrimeWheel } from './modules/PrimeWheel.mjs'
+//import { PrimeWheel } from './modules/PrimeWheel.mjs'
 import { SetColor } from './modules/SetColor.mjs'
 
 /*
  * Set up the terminal app.
  */
 cmdProcessor.addModule(new PostRenderer('api/posts.json'))
-cmdProcessor.addModule(new PrimeWheel({ 
+/*cmdProcessor.addModule(new PrimeWheel({ 
 	canvas_name: 'animation_canvas',
 	font_color: '#ff4500', font_size: '32px', font_face: 'Arial',
 	spam: true, debug: true
-}))
+}))*/
 cmdProcessor.addModule(new SetColor())
 
 //  Load posts.
