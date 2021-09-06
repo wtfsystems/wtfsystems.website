2373a91e658ba6ebb40226d12669ca65d30c2e7e
Matthew Evans <mevans+git@wtfsystems.net>
2021-09-05 21:47:34 -0400

    sync

--- docs/libwtf/search/all_1.js
+++ docs/libwtf/search/all_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['diamond_5fsquare_0',['diamond_square',['../classwtf_1_1diamond__square.html#af8a8d12ce3434e11325dea9435094cc2',1,'wtf::diamond_square::diamond_square(const std::size_t &amp;size, const long double &amp;offset, const uint64_t &amp;seed)'],['../classwtf_1_1diamond__square.html#ae51e88c05c603bfb1f299c70a3024d12',1,'wtf::diamond_square::diamond_square(const std::size_t &amp;size, const long double &amp;offset)'],['../classwtf_1_1diamond__square.html',1,'wtf::diamond_square']]]
+  ['diamond_5fsquare_0',['diamond_square',['../classwtf_1_1diamond__square.html#ab4e0ba62adf27a32c6fa5f6a50137e1c',1,'wtf::diamond_square::diamond_square(const std::size_t &amp;size, const long double &amp;offset, const uint32_t &amp;seed)'],['../classwtf_1_1diamond__square.html#ae51e88c05c603bfb1f299c70a3024d12',1,'wtf::diamond_square::diamond_square(const std::size_t &amp;size, const long double &amp;offset)'],['../classwtf_1_1diamond__square.html',1,'wtf::diamond_square']]]
 ];
