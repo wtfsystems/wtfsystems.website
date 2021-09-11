1a6ea38a0f10dcd1a4119e8fe64f52e2d7efbd0b
Matthew Evans <mevans+git@wtfsystems.net>
2021-09-10 20:41:46 -0400

    sync

--- docs/wtengine/search/all_11.js
+++ docs/wtengine/search/all_11.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['team_0',['team',['../classwte_1_1cmp_1_1team.html#a188cd5dc94f67df6b24daa73c61db69b',1,'wte::cmp::team::team()'],['../classwte_1_1cmp_1_1team.html',1,'wte::cmp::team']]],
   ['timer_1',['timer',['../structwte_1_1input_1_1lastkeypress.html#a2a01e5d121ca172744bc9050ca4435b5',1,'wte::input::lastkeypress::timer()'],['../structwte_1_1input_1_1lastbuttonpress.html#ab08849e538ccb4ca6792b5862d3ade2d',1,'wte::input::lastbuttonpress::timer()']]],
-  ['toggle_2',['toggle',['../classwte_1_1mnu_1_1toggle.html#a7f65d1ff95faeb656e57479f10580458',1,'wte::mnu::toggle::toggle()'],['../classwte_1_1mnu_1_1toggle.html',1,'wte::mnu::toggle']]],
+  ['toggle_2',['toggle',['../classwte_1_1mnu_1_1toggle.html#a30776232512c4f542056df7cd59c1c5c',1,'wte::mnu::toggle::toggle()'],['../classwte_1_1mnu_1_1toggle.html',1,'wte::mnu::toggle']]],
   ['toggle_5finput_5frecording_3',['toggle_input_recording',['../classwte_1_1input.html#ae9cb206be42399b543802054825979fd',1,'wte::input']]]
 ];
--- docs/wtengine/search/functions_10.js
+++ docs/wtengine/search/functions_10.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['team_0',['team',['../classwte_1_1cmp_1_1team.html#a188cd5dc94f67df6b24daa73c61db69b',1,'wte::cmp::team']]],
-  ['toggle_1',['toggle',['../classwte_1_1mnu_1_1toggle.html#a7f65d1ff95faeb656e57479f10580458',1,'wte::mnu::toggle']]],
+  ['toggle_1',['toggle',['../classwte_1_1mnu_1_1toggle.html#a30776232512c4f542056df7cd59c1c5c',1,'wte::mnu::toggle']]],
   ['toggle_5finput_5frecording_2',['toggle_input_recording',['../classwte_1_1input.html#ae9cb206be42399b543802054825979fd',1,'wte::input']]]
 ];
