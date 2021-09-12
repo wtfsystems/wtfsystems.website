b03ff4b4bbd3bec14e20238ccedbd59089029386
Matthew Evans <mevans+git@wtfsystems.net>
2021-09-12 08:18:14 -0400

    sync

--- docs/wtengine/search/all_2.js
+++ docs/wtengine/search/all_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['check_5finput_5fevents_0',['check_input_events',['../classwte_1_1input.html#a6f0ef42bf195ba08d2d09d2b51730563',1,'wte::input']]],
-  ['check_5ftime_1',['check_time',['../classwte_1_1engine__time.html#a47fabcc19077e7a3b3335d66d46fcdeb',1,'wte::engine_time']]],
+  ['check_0',['check',['../classwte_1_1engine__time.html#a02f1f8cc6dc39d9279eb8c35632791b1',1,'wte::engine_time']]],
+  ['check_5finput_5fevents_1',['check_input_events',['../classwte_1_1input.html#a6f0ef42bf195ba08d2d09d2b51730563',1,'wte::input']]],
   ['clear_5finstances_2',['clear_instances',['../structwte_1_1mgr_1_1audio_1_1sample.html#a663f2964c6764c6ed221324f06b50bfd',1,'wte::mgr::audio::sample']]],
   ['clear_5fsave_3',['clear_save',['../classwte_1_1mgr_1_1variables.html#a933ff801de0cc8339dd05e49b95bdd0d',1,'wte::mgr::variables']]],
   ['clear_5ftint_4',['clear_tint',['../classwte_1_1cmp_1_1gfx.html#ae3307c9f15d32a02435128246b5423d5',1,'wte::cmp::gfx']]],
