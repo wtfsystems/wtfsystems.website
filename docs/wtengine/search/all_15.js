bdaa4746c1dca7d2d2aac84311787738ba3f07cd
Matthew Evans <mevans+git@wtfsystems.net>
2021-10-30 10:28:58 -0400

    sync

--- docs/wtengine/search/all_15.js
+++ docs/wtengine/search/all_15.js
@@ -15,5 +15,5 @@ var searchData=
   ['wte_12',['wte',['../namespacewte.html',1,'']]],
   ['wte_5fasset_13',['wte_asset',['../namespacewte.html#afe8c5c490a75d19f3649cb7e7322af05',1,'wte']]],
   ['wte_5fexception_14',['wte_exception',['../classwte_1_1wte__exception.html',1,'wte::wte_exception'],['../classwte_1_1wte__exception.html#a1f3aa926405d22ff6b7e1b6d353c9319',1,'wte::wte_exception::wte_exception()']]],
-  ['wtebuildoptions_15',['WTEBUILDOPTIONS',['../struct_w_t_e_b_u_i_l_d_o_p_t_i_o_n_s.html',1,'']]]
+  ['wtebuildoptions_15',['WTEBUILDOPTIONS',['../structwte_1_1_w_t_e_b_u_i_l_d_o_p_t_i_o_n_s.html',1,'wte']]]
 ];
