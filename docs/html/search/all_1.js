980bb9b61c0f0641f849c7b5d5cacd8b090d9362
Matthew Evans <mevans+git@wtfsystems.net>
2022-06-16 00:05:22 -0400

    sync

--- docs/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['python_20web_20auth_20framework_0',['Python Web Auth Framework',['../index.html',1,'']]]
-];
--- docs/html/search/pages_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['libwtf_0',['libwtf',['../index.html',1,'']]],
-  ['license_1',['LICENSE',['../md__l_i_c_e_n_s_e.html',1,'']]]
-];
