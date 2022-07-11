cd1de30cf9960d06578ad1f3ac610b7844f33ebf
Matthew Evans <mevans+git@wtfsystems.net>
2022-07-11 00:37:46 -0400

    sync

--- _includes/leToucan.js
+++ _includes/leToucan.js
@@ -1,5 +1,13 @@
 /*
  * Le Toucan Has Arrived!
+ *
+ * Filename:  leToucan.js
+ * By:  Matthew Evans
+ *      https://www.wtfsystems.net/
+ * Version:  42
+ *
+ * Copyright (c) 2022 Matthew Evans - See LICENSE.md
+ *
  */
 const leData = {
     leProcess: null,
@@ -21,7 +29,7 @@ const leData = {
 const leSpam = (newInterval) => {
     if(newInterval !== undefined) leData.leInterval = newInterval
     leData.leProcess = setInterval(() => { 
-        console.log(`You have entered trade chat ${Math.floor(Math.random() * leData.leInterval)}`)
+        console.log(`You have entered trade chat ${Math.floor(Math.random() * 999)}`)
         console.log(leData.leToucan)
     }, leData.leInterval)
 }
