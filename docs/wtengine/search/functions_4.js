01a8011f222d6488a105e731a201283adf788713
Matthew Evans <mevans+git@wtfsystems.net>
2022-10-26 15:46:07 -0400

    sync

--- docs/wtengine/search/functions_4.js
+++ docs/wtengine/search/functions_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['end_5fgame_0',['end_game',['../classwte_1_1engine.html#a1fb5c6cfcfcb687d31f345eea80d3a85',1,'wte::engine']]],
-  ['engine_1',['engine',['../classwte_1_1engine.html#a46825a0534191e328fb5906756186648',1,'wte::engine::engine(const int &amp;argc, char **const &amp;argv)'],['../classwte_1_1engine.html#a834ce10430ebe328a4045b4cef339650',1,'wte::engine::engine(const engine &amp;)=delete']]],
+  ['engine_1',['engine',['../classwte_1_1engine.html#a834ce10430ebe328a4045b4cef339650',1,'wte::engine::engine(const engine &amp;)=delete'],['../classwte_1_1engine.html#a46825a0534191e328fb5906756186648',1,'wte::engine::engine(const int &amp;argc, char **const &amp;argv)']]],
   ['engine_5ferror_2',['engine_error',['../classwte_1_1engine__error.html#a87de7cf43bf9d1133e7c80d45d3fe8b2',1,'wte::engine_error::engine_error(const std::string &amp;d)'],['../classwte_1_1engine__error.html#a2508d36d83a5aa5e8623a4977f0555ef',1,'wte::engine_error::engine_error()=delete']]],
   ['engine_5fexception_3',['engine_exception',['../classwte_1_1engine__exception.html#aae1478a16618fc3790e181231c4b30ee',1,'wte::engine_exception::engine_exception(const std::string &amp;d, const std::string &amp;l, const uint &amp;c)'],['../classwte_1_1engine__exception.html#afcef1b19953b7ae6395479909f2fba15',1,'wte::engine_exception::engine_exception()=delete']]],
   ['engine_5ftime_4',['engine_time',['../classwte_1_1engine__time.html#a50715bf55cbe8084ab7d4a14cc2b40c3',1,'wte::engine_time::engine_time()=delete'],['../classwte_1_1engine__time.html#a4f0405e0a04fe25db3db6c311888197d',1,'wte::engine_time::engine_time(const engine_time &amp;)=delete']]],
