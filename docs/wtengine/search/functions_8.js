47d54861f83810515f17d8230e0be2760efa7d49
Matthew Evans <mevans+git@wtfsystems.net>
2022-11-11 15:23:38 -0500

    sync

--- docs/wtengine/search/functions_8.js
+++ docs/wtengine/search/functions_8.js
@@ -4,6 +4,7 @@ var searchData=
   ['load_5fscript_1',['load_script',['../classwte_1_1mgr_1_1messages.html#ad5d3786b4bf4c232df8003cd08dc95e7',1,'wte::mgr::messages']]],
   ['load_5fsystems_2',['load_systems',['../classwte_1_1engine.html#a2fcc4367a6ead89ae6fb95e080d5a134',1,'wte::engine']]],
   ['location_3',['location',['../classwte_1_1cmp_1_1location.html#a73f311698773e441cbeb49f39f09ee37',1,'wte::cmp::location::location(const float &amp;x, const float &amp;y)'],['../classwte_1_1cmp_1_1location.html#a4ea23bff4e251707d96203a296b7ffe0',1,'wte::cmp::location::location()=delete']]],
-  ['logger_4',['logger',['../classwte_1_1logger.html#afbdec474a98c42d8f286e7e5a42c7d99',1,'wte::logger']]],
-  ['loop_5',['loop',['../structwte_1_1mgr_1_1audio_1_1music_1_1a.html#a1bcdc4fcf87f553a6a2e6cf49d0320a3',1,'wte::mgr::audio::music::a::loop()'],['../structwte_1_1mgr_1_1audio_1_1music_1_1b.html#a56be92a95b7de89bc319ccb20a6779d6',1,'wte::mgr::audio::music::b::loop()'],['../structwte_1_1mgr_1_1audio_1_1ambiance.html#a17ef6626738ab6949de5585d2324e1ec',1,'wte::mgr::audio::ambiance::loop()']]]
+  ['log_4',['log',['../classwte_1_1logger.html#a099d4bb398ea58d285adc784ec8b416a',1,'wte::logger']]],
+  ['logger_5',['logger',['../classwte_1_1logger.html#afbdec474a98c42d8f286e7e5a42c7d99',1,'wte::logger']]],
+  ['loop_6',['loop',['../structwte_1_1mgr_1_1audio_1_1music_1_1a.html#a1bcdc4fcf87f553a6a2e6cf49d0320a3',1,'wte::mgr::audio::music::a::loop()'],['../structwte_1_1mgr_1_1audio_1_1music_1_1b.html#a56be92a95b7de89bc319ccb20a6779d6',1,'wte::mgr::audio::music::b::loop()'],['../structwte_1_1mgr_1_1audio_1_1ambiance.html#a17ef6626738ab6949de5585d2324e1ec',1,'wte::mgr::audio::ambiance::loop()']]]
 ];
