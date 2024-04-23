eaf75dff93c80cd86abee5abadf114e847c13584
Matthew Evans <mevans+git@wtfsystems.net>
2024-04-23 11:10:57 -0400

    sync

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,7 @@
     <img src="/img/wtfsystems.png" width="90%">
   </p>
   <p style="text-align: center;">
-    If page does not redirect please visit
-    <a href="https://github.com/wtfsystems">https://github.com/wtfsystems</a>.
+    <a href="https://github.com/wtfsystems">https://github.com/wtfsystems</a>
   </p>
 </template>
 
