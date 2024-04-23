98e5ae26fbf5b1a9a1b47163b087de6495cafdba
Matthew Evans <mevans+git@wtfsystems.net>
2024-04-23 11:07:31 -0400

    sync

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <p style="text-align: center;">
+    <img src="/img/wtfsystems.png" width="90%">
+  </p>
+  <p style="text-align: center;">
+    If page does not redirect please visit
+    <a href="https://github.com/wtfsystems">https://github.com/wtfsystems</a>.
+  </p>
+</template>
+
+<style lang="stylus" scoped>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './style.styl'
+
+createApp(App).mount('#app')
