47399ec374ae13700da84096b4615b7858bcb681
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-24 07:43:56 -0400

    sync

--- src/components/WTGui.vue
+++ src/components/WTGui.vue
@@ -27,7 +27,7 @@ const links = [
     <a href="https://github.com/wtfsystems/wtgui">
       <img src="/src/assets/wtgui-logo.png">
     </a>
-    <h2>Experimental HTML5 Canvas Menu System</h2>
+    <h2>Experimental Vue3 Menu System</h2>
     <ul v-for="item in links">
       <li>{{ item.title }} <a :href="item.url">{{ item.url }}</a></li>
     </ul>
